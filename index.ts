f76562a5e20dcfdb2ba40b5f80fdad019b2c2c79
Paweł Blajer <pawel.krzysztof.blajer@intel.com>
2025-05-26 14:42:33 +0200

    example 7

--- index.ts
+++ index.ts
@@ -1,8 +1,27 @@
-// 6. tuples
+// 7. exending module
 
-function process(successfully: boolean): [boolean, string] {
-    if (!successfully)
-        return [false, 'failed'];
-    return [true, 'processed'];
+//const Koa = require('koa');
+//const log4js = require('log4js');
+import Koa from 'koa';
+import * as log4js from 'log4js';
+
+log4js.configure({
+    appenders: { myConsoleAppender: { type: 'console' } },
+    categories: { default: { appenders: ['myConsoleAppender'], level: log4js.levels.DEBUG.levelStr }}
+});
+declare module 'koa' {
+    interface Context {
+        log: log4js.Logger;
+    }
 }
-const [isSuccess, message] = process(true);
+const app = new Koa();
+app.use(async (ctx: Koa.Context, next: Koa.Next) => {
+    ctx.log = log4js.getLogger();
+    return next();
+});
+app.use(async function(ctx: Koa.Context) {
+    ctx.log.info('hello');
+    ctx.status = 200;
+    ctx.body = 'yay';
+});
+app.listen(8080);
